172710b338cb6c07cdefad51f98ab69e6ffb94bf
Ryuuzan <alexmilo96@wanadoo.fr>
2018-06-03 18:53:44 +0200

    New commands
    -Improved message check
    -First version Say Command

--- bot.js
+++ bot.js
@@ -1,17 +1,53 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+
+
 client.on('message', message => {
-    if (message.content === 'test') {
-    	message.delete(0)
-        .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-        .catch(console.error);
-        message.reply('Bonjour, veuillez respecter les règles de la communauté');
-  	}
+	var res = message.content.split(" ");
+	//~say [couleur] [titre] [msg] 
+	if(res[0] === "~say")
+	{
+		if(res.length == 4)
+			{ 
+				if(res[2]==="bleu")
+				{
+					message.channel.send({embed: {
+  						color: 255,
+  						title: res[2]
+  						description: "A very simple Embed!"
+					}});
+				}
+			}
+			else
+			{
+				message.channel.send({embed: {
+  					color: 16777215,
+  					title: res[2]
+  					description: "A very simple Embed!"
+				}});
+			}
+	}
+	else{
+
+	//Read each word so that it can find insults and doesn't care about casse
+	res.forEach(function(element) {
+  	if (element.toLowerCase() === 'test') {
+    		message.delete(0)
+        	.then(msg => console.log(`Deleted message from ${msg.author.username}`))
+        	.catch(console.error);
+        	message.reply('Bonjour, veuillez respecter les règles de la communauté');
+  		}
+	});
+}
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
